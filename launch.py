ccc5e72cbb4d7c0bc241687c3ad9477e81fa7c85
dppsencik <dppsencik@gmail.com>
2023-10-27 12:18:21 -0500

    Now handles invalid paths better for user PEP8 Formatting

--- launch.py
+++ launch.py
@@ -10,19 +10,22 @@ BASEDIR = os.path.dirname(__file__)
 
 try:
     from ctypes import windll  # Only exists on Windows.
-    APP_ID = 'Foundry.Katana.Launcher.1'
+
+    APP_ID = "Foundry.Katana.Launcher.1"
     windll.shell32.SetCurrentProcessExplicitAppUserModelID(APP_ID)
 except ImportError:
     pass
 
+
 class KatanaLauncher(QtWidgets.QMainWindow):
     """class representing the main launcher UI"""
+
     def __init__(self):
         super(KatanaLauncher, self).__init__()
-        loadUi(os.path.join(BASEDIR,'assets\\KatanaLauncher.ui'), self)
-        self.setWindowIcon(QtGui.QIcon(os.path.join(BASEDIR, 'assets\\Katana.ico')))
+        loadUi(os.path.join(BASEDIR, "assets\\KatanaLauncher.ui"), self)
+        self.setWindowIcon(QtGui.QIcon(os.path.join(BASEDIR, "assets\\Katana.ico")))
         self.config = configparser.ConfigParser()
-        self.config.read(os.path.join(BASEDIR,'config.ini'))
+        self.config.read(os.path.join(BASEDIR, "config.ini"))
         self.populate()
         self.refresh_BTN.clicked.connect(self.populate)
         self.renderer_CB.currentTextChanged.connect(self.renderer_changed)
@@ -44,21 +47,29 @@ class KatanaLauncher(QtWidgets.QMainWindow):
         katana_line = self.katana_version_CB.currentText()[0:3]
         self.renderer_version_CB.clear()
         self.renderer_version_CB.setEnabled(False)
-        if index == '3Delight':
-            self.renderer_version_CB.addItem('Katana Version')
+        if index == "3Delight":
+            self.renderer_version_CB.addItem("Katana Version")
             self.renderer_version_CB.setCurrentIndex(0)
-        elif index == 'RenderMan':
-            renderman_path = self.config.get('RenderMan', 'Path')
-            renderman_versions = [file.split('-')[1] for file in os.listdir(renderman_path)
-                if 'RenderManForKatana' in file and os.path.isdir(renderman_path + '\\' + file)]
+        elif index == "RenderMan":
+            renderman_path = self.config.get("RenderMan", "Path")
+            renderman_versions = [
+                file.split("-")[1]
+                for file in os.listdir(renderman_path)
+                if "RenderManForKatana" in file
+                and os.path.isdir(renderman_path + "\\" + file)
+            ]
             renderman_versions.reverse()
             self.renderer_version_CB.addItems(renderman_versions)
             self.renderer_version_CB.setEnabled(True)
-        elif index == 'Arnold':
-            arnold_path = self.config.get('Arnold', "Path")
-            arnold_versions = [file.split('-')[1] for file in os.listdir(arnold_path)
-                if 'kat' + katana_line in file and 'ktoa' in file and
-                os.path.isdir(arnold_path + '\\' + file)]
+        elif index == "Arnold":
+            arnold_path = self.config.get("Arnold", "Path")
+            arnold_versions = [
+                file.split("-")[1]
+                for file in os.listdir(arnold_path)
+                if "kat" + katana_line in file
+                and "ktoa" in file
+                and os.path.isdir(arnold_path + "\\" + file)
+            ]
             arnold_versions.reverse()
             self.renderer_version_CB.addItems(arnold_versions)
             self.renderer_version_CB.setEnabled(True)
@@ -76,44 +87,79 @@ class KatanaLauncher(QtWidgets.QMainWindow):
         # clear versions
         self.katana_version_CB.clear()
         self.renderer_CB.clear()
-        katana_path = self.config.get('Katana', "Path")
-        katana_versions = [file[6:] for file in os.listdir(katana_path)
-                           if os.path.isfile(katana_path + '\\'  + file + '\\bin\\katanaBin.exe')]
-        renderers = [file[0:-4] for file in os.listdir((os.path.join(BASEDIR,'scripts\\Renderers')))
-                     if file.endswith('.bat')]
-        scripts = [file[0:-4] for file in os.listdir((os.path.join(BASEDIR,'scripts')))
-                     if file.endswith('.bat')]
-        #get scripts
-        for script in scripts:
-            check_box = QtWidgets.QCheckBox(script)
-            self.scripts_layout.addWidget(check_box)
-        self.scripts_layout.addStretch()
 
-        #get versions
-        katana_versions.reverse()
-        self.katana_version_CB.addItems(katana_versions)
-        self.renderer_CB.addItems(renderers)
+        self.config.read(os.path.join(BASEDIR, "config.ini"))
+        if self.validate_paths():
+            # get versions
+            katana_path = self.config.get("Katana", "Path")
+            katana_versions = [
+                file[6:]
+                for file in os.listdir(katana_path)
+                if os.path.isfile(katana_path + "\\" + file + "\\bin\\katanaBin.exe")
+            ]
+            renderers = [
+                file[0:-4]
+                for file in os.listdir((os.path.join(BASEDIR, "scripts\\Renderers")))
+                if file.endswith(".bat")
+            ]
+            scripts = [
+                file[0:-4]
+                for file in os.listdir((os.path.join(BASEDIR, "scripts")))
+                if file.endswith(".bat")
+            ]
+            # get scripts
+            for script in scripts:
+                check_box = QtWidgets.QCheckBox(script)
+                self.scripts_layout.addWidget(check_box)
+            self.scripts_layout.addStretch()
+
+            # add to UI
+            katana_versions.reverse()
+            self.katana_version_CB.addItems(katana_versions)
+            self.renderer_CB.addItems(renderers)
 
     def launch(self):
         """Gather all environment variables, combine, and run a single .bat, launching Katana"""
+        # Gather user input
         katana_version = self.katana_version_CB.currentText()
-        os.environ['KATANA_VERSION'] = katana_version
-        os.environ['KATANA_LINE'] = katana_version[:3]
-        os.environ['KATANA_ROOT'] = self.config.get('Katana', "Path") + '\\Katana' + katana_version
-        os.environ['RENVER'] = self.renderer_version_CB.currentText()
+        os.environ["KATANA_VERSION"] = katana_version
+        os.environ["KATANA_LINE"] = katana_version[:3]
+        os.environ["KATANA_ROOT"] = (
+            self.config.get("Katana", "Path") + "\\Katana" + katana_version
+        )
+        os.environ["RENVER"] = self.renderer_version_CB.currentText()
         renderer = self.renderer_CB.currentText()
+        # Sanity check inputs
+        print("KATANA VERSION: " + katana_version)
+        print("RENDERER VERSION: " + renderer)
+
+        if katana_version == "" or renderer == "":
+            QtWidgets.QMessageBox.critical(
+                None,
+                "Error",
+                "No Katana version or renderer selected.",
+                QtWidgets.QMessageBox.Ok,
+            )
+            return
+        # Create and launch script
         optional_scripts = (
-            self.scripts_layout.itemAt(i).widget() for i in range(self.scripts_layout.count()-1)
-            if self.scripts_layout.itemAt(i).widget().isChecked())
-        cmd = ''
+            self.scripts_layout.itemAt(i).widget()
+            for i in range(self.scripts_layout.count() - 1)
+            if self.scripts_layout.itemAt(i).widget().isChecked()
+        )
+        cmd = ""
         for script in optional_scripts:
-            cmd += Path(os.path.join(BASEDIR,'scripts\\' + script.text() + '.bat')).read_text(encoding="utf-8")
-        cmd += Path(os.path.join(BASEDIR,'scripts\\Renderers\\' + renderer + '.bat')).read_text(encoding="utf-8")
-        cmd += "\n\"%KATANA_ROOT%\\bin\\katanaBin.exe\""
+            cmd += Path(
+                os.path.join(BASEDIR, "scripts\\" + script.text() + ".bat")
+            ).read_text(encoding="utf-8")
+        cmd += Path(
+            os.path.join(BASEDIR, "scripts\\Renderers\\" + renderer + ".bat")
+        ).read_text(encoding="utf-8")
+        cmd += '\n"%KATANA_ROOT%\\bin\\katanaBin.exe"'
         # Temp bat file deletes itself when Katana closes
-        cmd += "\ngoto 2>nul & del \"%~f0\""
+        cmd += '\ngoto 2>nul & del "%~f0"'
         # Create temporary file with all commands
-        with open(os.path.join(BASEDIR,"temp.bat"), "w", encoding="utf-8") as f:
+        with open(os.path.join(BASEDIR, "temp.bat"), "w", encoding="utf-8") as f:
             f.write(cmd)
 
         os.system("start cmd /c temp.bat")
@@ -121,35 +167,38 @@ class KatanaLauncher(QtWidgets.QMainWindow):
     def edit_scripts(self):
         """Open a notepad of user selected scripts to be edited"""
         optional_scripts = (
-            self.scripts_layout.itemAt(i).widget() for i in range(self.scripts_layout.count()-1)
-             if self.scripts_layout.itemAt(i).widget().isChecked())
+            self.scripts_layout.itemAt(i).widget()
+            for i in range(self.scripts_layout.count() - 1)
+            if self.scripts_layout.itemAt(i).widget().isChecked()
+        )
         for script in optional_scripts:
-            subprocess.Popen("notepad.exe" + (os.path.join(BASEDIR, "scripts/" + script.text() + '.bat')))
-
+            subprocess.Popen(
+                "notepad.exe "
+                + os.path.join(BASEDIR, "scripts/" + script.text() + ".bat")
+            )
 
     def open_settings(self):
         """Open the settings config notepad"""
-        subprocess.Popen("notepad.exe config.ini")
+        subprocess.Popen("notepad.exe " + os.path.join(BASEDIR, "config.ini"))
 
     def validate_paths(self):
         """Ensure all paths in config file exist"""
         for section in self.config.sections():
-            path = self.config.get(section, 'Path')
+            path = self.config.get(section, "Path")
             if not os.path.exists(path):
+                QtWidgets.QMessageBox.critical(
+                    None,
+                    "Error",
+                    "One or more of your paths are not valid, please modify your config.ini file.",
+                    QtWidgets.QMessageBox.Ok,
+                )
                 return False
         return True
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     ui = KatanaLauncher()
-    if ui.validate_paths():
-        ui.center_on_screen()
-        ui.show()
-        app.exec_()
-    else:
-        QtWidgets.QMessageBox.critical(
-            None,"Error",
-            "One or more of your paths are not valid, please modify your config.ini file.",
-            QtWidgets.QMessageBox.Ok)
-        app.exit(0)
-    
+    ui.center_on_screen()
+    ui.show()
+    app.exec_()
